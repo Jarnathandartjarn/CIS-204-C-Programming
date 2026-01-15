4ec7ec0606ca29bc8eb296f41828c58de547e07f
Jarnathandartjarn <amazonbox1998@gmail.com>
2026-01-15 00:23:08 +0000

    Version 1

---
+++ Week 1/Favorites.c
@@ -0,0 +1,13 @@
+// Jonathan Falvo
+// Chpt 1 PA
+#include <stdio.h>
+
+int main(){
+    
+    printf("My name is Jonathan Falvo.\n");
+    printf("My favorite song is I Still Haven't Found What I'm Looking For by U2.\n");
+    printf("My favorite movie is Star Wars: Revenge of the Sith.\n");
+    printf("My favorite food is currently homemade burgers.\n");
+
+    return 0; // Program is succesful
+}
