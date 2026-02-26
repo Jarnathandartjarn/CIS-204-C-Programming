042f1d61fbaec7adfc09e9313ce4057067f72b78
Jarnathandartjarn <amazonbox1998@gmail.com>
2026-02-26 01:26:43 +0000

    Week 7, Wednesday Updates

--- Week 7/w7n2w1.c
+++ Week 7/w7n2w1.c
@@ -2,8 +2,7 @@
 
 int main () {
 
-for(int j = 3; j > 0; --j) {            /* j or outer number is equal to 3, j is greater than 0 and will operate to 1,
-                                           j will "decrement" each iteration of the loop */
+for(int j = 3; j > 0; --j) {
     for(int i = 3; i > 0; --i) {
     printf("%d-%d\n", j, i); 
 }
